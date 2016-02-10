040905f4bbaa6fabc069ffda0fc5f4f41fc1cecd
Milko Škofič <info@skofic.net>
2016-02-10 01:10:01 +0100

    Added documentation.

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","ArrayAccess"],["c","ArrayObject"],["c","BadFunctionCallException"],["c","BadMethodCallException"],["c","Countable"],["c","Exception"],["c","InvalidArgumentException"],["c","IteratorAggregate"],["c","LogicException"],["c","Milko\\PHPLib\\Container"],["c","Milko\\PHPLib\\DatabaseServer"],["c","Milko\\PHPLib\\DataSource"],["c","Milko\\PHPLib\\Server"],["c","RuntimeException"],["c","Serializable"],["c","Throwable"],["c","Traversable"]];
