28dc47986d45609a7b9146a4193f212a8ff399f6
Milko Škofič <info@skofic.net>
2016-04-12 18:55:53 +0200

    Finalised Server test suite, may need to add tests.

--- test/Server.php
+++ test/Server.php
@@ -3,15 +3,27 @@
 /**
  * Server object test suite.
  *
+ * This test suite will use a ficticious test class, to perform more in depth test use the
+ * concrete classes derived from DataServer.
+ *
  *	@author		Milko A. Škofič <skofic@gmail.com>
  *	@version	1.00
- *	@since		06/02/2016
+ *	@since		18/02/2016
  */
+
+//
+// Global definitions.
+//
+define( 'kENGINE', "MONGO" );
 
 //
 // Include local definitions.
 //
 require_once(dirname(__DIR__) . "/includes.local.php");
+if( kENGINE == "MONGO" )
+	require_once(dirname(__DIR__) . "/mongo.local.php");
+elseif( kENGINE == "ARANGO" )
+	require_once(dirname(__DIR__) . "/arango.local.php");
 
 //
 // Include utility functions.
@@ -19,105 +31,113 @@ require_once(dirname(__DIR__) . "/includes.local.php");
 require_once( "functions.php" );
 
 //
-// Reference class.
+// Enable exception logging.
 //
-use Milko\PHPLib\Server;
-
-//
-// Test class.
-//
-class test_Server extends Milko\PHPLib\Server
-{
-	//
-	// Implement virtual interface.
-	//
-	function connectionCreate( $theOptions = NULL ) {
-		return "I am open!";	}
-	function connectionDestruct( $theOptions = NULL ) {
-		$this->mConnection = "I am closed."; }
-}
+//ArangoException::enableLogging();
 
 //
 // Instantiate object.
 //
-echo( '$url = "protocol://user:pass@host:9090/path";' . "\n" );
-$url = "protocol://user:pass@host:9090/path";
-echo( '$test = new test_Server( $url' . " );\n\n" );
-$test = new test_Server( $url );
-
-//
-// Retrieve connection.
-//
-echo( "Retrieve connection:\n" );
-echo( '$result = $test->isConnected();' . "\n" );
-$result = dumpValue( $test->isConnected() );
-echo( "Result: $result\n" );
-echo( '$result = $test->Connection();' . "\n" );
-$result = dumpValue( $test->Connection() );
-echo( "Result: $result\n" );
-
-echo( "\n" );
-
-//
-// Connect.
-//
-echo( "Connect:\n" );
-echo( '$result = $test->Connect();' . "\n" );
-$result = dumpValue( $test->Connect() );
-echo( "Result: $result\n" );
-echo( '$result = $test->isConnected();' . "\n" );
-$result = dumpValue( $test->isConnected() );
-echo( "Result: $result\n" );
+if( kENGINE == "MONGO" )
+{
+	echo( '$test = new \Milko\PHPLib\MongoDB\Server();' . "\n" );
+	$test = new \Milko\PHPLib\MongoDB\Server();
+	echo( '$result = (string)$test;' . "\n" );
+	echo( (string)$test . ' ==> ' );
+	echo( ( "$test" == kMONGO_OPTS_CLIENT_DEFAULT ) ? "OK\n" : "FALIED\n" );
+}
+elseif( kENGINE == "ARANGO" )
+{
+	echo( '$test = new \Milko\PHPLib\ArangoDB\Server();' . "\n" );
+	$test = new \Milko\PHPLib\ArangoDB\Server();
+	echo( '$result = (string)$test;' . "\n" );
+	echo( (string)$test . ' ==> ' );
+	echo( ( "$test" == kARANGO_OPTS_CLIENT_DEFAULT ) ? "OK\n" : "FALIED\n" );
+}
 
 echo( "\n" );
 
 //
-// Disconnect.
+// Instantiate object.
 //
-echo( "Disconnect:\n" );
-echo( '$result = $test->Disconnect();' . "\n" );
-$result = dumpValue( $test->Disconnect() );
-echo( "Result: $result\n" );
-echo( '$result = $test->isConnected();' . "\n" );
-$result = dumpValue( $test->isConnected() );
-echo( "Result: $result\n" );
-echo( '$result = $test->Disconnect();' . "\n" );
-$result = dumpValue( $test->Disconnect() );
-echo( "Result: $result\n" );
+if( kENGINE == "MONGO" )
+{
+	echo( '$url = "mongodb://localhost:27017/test_milkolib";' . "\n" );
+	$url = "mongodb://localhost:27017/test_milkolib";
+	echo( '$test = new \Milko\PHPLib\MongoDB\Server( $url' . " );\n" );
+	$test = new \Milko\PHPLib\MongoDB\Server( $url );
+	echo( '$result = (string)$test;' . "\n" );
+	echo( (string)$test . " ==> " );
+	echo( ( "$test" == $url ) ? "OK\n" : "FALIED\n" );
+}
+elseif( kENGINE == "ARANGO" )
+{
+	echo( '$url = "tcp://localhost:8529/test_milkolib";' . "\n" );
+	$url = "tcp://localhost:8529/test_milkolib";
+	echo( '$test = new \Milko\PHPLib\ArangoDB\Server( $url' . " );\n" );
+	$test = new \Milko\PHPLib\ArangoDB\Server( $url );
+	echo( '$result = (string)$test;' . "\n" );
+	echo( (string)$test . " ==> " );
+	echo( ( "$test" == $url ) ? "OK\n" : "FALIED\n" );
+}
 
 echo( "\n====================================================================================\n\n" );
 
 //
-// Change protocol.
+// List server databases.
 //
-echo( "Change protocol:\n" );
-echo( '$test->Protocol( "Someotherprotocol" );' . "\n" );
-$test->Protocol( "Someotherprotocol" );
-echo( "==> $test\n" );
+echo( "List databases:\n" );
+echo( '$list = $test->ListDatabases();' . "\n" );
+$list = $test->ListDatabases();
+print_r( $list );
+
+echo( "\n====================================================================================\n\n" );
+
+//
+// Retrieve database.
+//
+echo( "Retrieve database:\n" );
+echo( '$db = $test->GetDatabase( "test_milkolib" );' . "\n" );
+$db = $test->GetDatabase( "test_milkolib" );
+echo( get_class( $db ) . "\n" );
+echo( "$db ==> " );
+echo( ( "$db" == "test_milkolib" ) ? "OK\n" : "FALIED\n" );
 
 echo( "\n" );
 
 //
-// Reconnect.
+// Retrieve non existing database.
 //
-echo( "Reconnect:\n" );
-echo( '$result = $test->Connect();' . "\n" );
-$result = dumpValue( $test->Connect() );
-echo( "Result: $result\n" );
+echo( "Retrieve non existing database:\n" );
+echo( '$db = $test->GetDatabase( "UNKNOWN" );' . "\n" );
+$db = $test->GetDatabase( "UNKNOWN" );
+var_dump( $db );
+echo( "$db ==> " );
+echo( ( $db === NULL ) ? "OK\n" : "FALIED\n" );
+
+echo( "\n====================================================================================\n\n" );
+
+//
+// Create database.
+//
+echo( "Create database:\n" );
+echo( '$db = $test->NewDatabase( "NewDB" );' . "\n" );
+$db = $test->NewDatabase( "NewDB" );
+echo( '$list = $test->ListDatabases();' . "\n" );
+$list = $test->ListDatabases();
+print_r( $list );
 
 echo( "\n" );
 
 //
-// Change protocol.
+// Drop database.
 //
-echo( "Change protocol, should raise an exception:\n" );
-echo( '$test->Protocol( "html" );' . "\n" );
-try{ $test->Protocol( "html" ); echo( "Failed!\n" ); }
-catch( Exception $error ){ echo( $error->getMessage() . "\n" ); }
-echo( '$test[ Milko\PHPLib\Server::PROT ] = "HTML";' . "\n" );
-try{ $test[ Milko\PHPLib\Server::PROT ] = "HTML"; echo( "Failed!\n" ); }
-catch( Exception $error ){ echo( $error->getMessage() . "\n" ); }
+echo( "Drop database:\n" );
+echo( '$db = $test->DelDatabase( "test_milkolib" );' . "\n" );
+$db = $test->DelDatabase( "test_milkolib" );
+echo( '$list = $test->ListDatabases();' . "\n" );
+$list = $test->ListDatabases();
+print_r( $list );
 
 
 ?>
-
